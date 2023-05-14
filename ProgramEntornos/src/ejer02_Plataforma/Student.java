d4be25f4719a46fb738ad0d9cd695999e1ec17fb
Mario Tarus <mariotarus77@gmail.com>
2023-05-14 13:04:29 +0200

    Merge remote-tracking branch 'origin/master'

--- ProgramEntornos/src/ejer02_Plataforma/Student.java
+++ ProgramEntornos/src/ejer02_Plataforma/Student.java
@@ -3,7 +3,7 @@ package ejer02_Plataforma;
 public class Student extends Person{
     private float avGrade;
 private String aka, email;
-    public Student(String name, String surname, String address, int phoneNumber, String aka, String email, float avGrade) {
+    public Student(String name, String surname, String address, int phoneNumber, String aka, String email) {
         super(name, surname, address, phoneNumber);
         this.aka = aka;
         this.email = email;
